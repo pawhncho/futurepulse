a96baada47e7af3ab5c1018d649e8eb8a879eebd
pawhncho <pawhncho@gmail.com>
2025-04-06 09:50:39 -0700

    Init

--- project/settings.py
+++ project/settings.py
@@ -162,15 +162,15 @@ EMAIL_HOST = 'smtp.gmail.com'
 
 EMAIL_PORT = 587
 
-<<<<<<< HEAD
+
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-=======
+
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') or ''
 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') or ''
->>>>>>> eec716c103f47da60a52151e88a20c8e17b63119
+
 
 EMAIL_USE_TLS = True
 
