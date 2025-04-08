71203b6e077fa45660aaaa58118910b94e1b413c
pawhncho <pawhncho@gmail.com>
2025-04-08 03:28:19 -0700

    Init

--- project/settings.py
+++ project/settings.py
@@ -179,7 +179,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)], # If deploying on Render, use your Redis URL instead of 127.0.0.1
+            'hosts': [os.environ.get('REDIS_URL')], # If deploying on Render, use your Redis URL instead of 127.0.0.1
         },
     },
 }
